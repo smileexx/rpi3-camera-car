d5e82d3a2a3d0ce296cf4c3141132539cb0fc50a
smileexx <smilee900@gmail.com>
2018-10-24 01:53:12 +0300

    Car: * add GPIO usage

--- src/car.py
+++ src/car.py
@@ -1,4 +1,4 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import threading
 from time import sleep
 import socketserver as SocketServer
@@ -6,8 +6,12 @@ from server import MyTCPHandler
 
 lock = threading.Lock()
 
+MM_L_PIN = 17    # Move motor level pin PWM
 
 class Car:
+    MM = None
+    MM_DC = 0
+    
     key_up = False
     key_down = False
     key_left = False
@@ -17,20 +21,19 @@ class Car:
     mask = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "Shift", "Control"]
 
     def __init__(self):
+        print("init hardware")
+        self.init_pins()
+        
+        print("init server")
         server_address = ("0.0.0.0", 46464)
         server = SocketServer.TCPServer(server_address, MyTCPHandler)
         server._car = self
         self._server = server
-
+        
         print("init car")
-        self.reset_gpio()
         self.run_server()
         self.run()
-
-    def reset_gpio(self):
-        # reset all GPIO pins
-        pass
-
+        
     def run(self):
         print("Car run")
         while self.loop:
@@ -39,9 +42,11 @@ class Car:
                 sleep(0.1)  # 100ms
             except KeyboardInterrupt:
                 self.loop = False
+                self.reset_gpio()
                 return 0
             except Exception as ex:
                 print("ERROR in Car loop. %s" % ex)
+                self.reset_gpio()
 
     def run_server(self):
         print("Server run")
@@ -61,10 +66,12 @@ class Car:
 
         if not (self.key_up or self.key_down or self.key_left or self.key_right):
             # print("Stop all engines")
+            self.move_motor()
             return 0
 
         if self.key_up:
             print("Car forward")
+            self.move_motor(1)
         elif self.key_down:
             print("Car move back")
 
@@ -89,3 +96,31 @@ class Car:
         for index, key in enumerate(self.mask):
             self.change_state(key, bool(data[index] == '1'))
         lock.release()
+       
+    def init_pins(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)        
+        
+        GPIO.setup(MM_L_PIN, GPIO.OUT)
+        self.MM = GPIO.PWM(MM_L_PIN, 100)
+       
+    def reset_gpio(self):
+        GPIO.cleanup()
+       
+    def move_motor(self, pow = 0):
+        if pow:
+            if self.MM_DC <= 10:
+                self.MM_DC = 20
+                self.MM.start(10)
+                sleep(0.2)
+            elif self.MM_DC <= 20:
+                self.MM_DC = 50
+                self.MM.start(20)
+                sleep(0.2)
+            else:
+                self.MM.ChangeDutyCycle(self.MM_DC)
+        else:
+            self.MM_DC = 0
+            self.MM.stop()
+
+    
