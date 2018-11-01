743f39a084af7a8d86d30756e65c4a4356068fc5
smileexx <smilee900@gmail.com>
2018-11-02 00:48:29 +0200

    fix

--- src/server/main.py
+++ src/server/main.py
@@ -4,7 +4,7 @@ from time import sleep
 import os
 
 if __name__ == "__main__":
-    dir_path = os.path.dirname(__file__)
+    dir_path = os.path.dirname(os.path.abspath(__file__))
     cmd_ffserv = 'ffserver -f ' + dir_path + '/../../camera/ffserver-mjpeg.conf'
     cmd_ff = 'ffmpeg -f video4linux2 -s 640x480 -r 15 -i /dev/video0 http://localhost:8090/camera.ffm'
     print(cmd_ffserv)
