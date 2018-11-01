4173d8c27e4763b47a2500ebca56f7b345e57b55
smileexx <smilee900@gmail.com>
2018-11-02 00:46:01 +0200

    fix

--- src/server/main.py
+++ src/server/main.py
@@ -5,12 +5,14 @@ import os
 
 if __name__ == "__main__":
     dir_path = os.path.dirname(__file__)
+    cmd_ffserv = 'ffserver -f ' + dir_path + '/../../camera/ffserver-mjpeg.conf'
+    cmd_ff = 'ffmpeg -f video4linux2 -s 640x480 -r 15 -i /dev/video0 http://localhost:8090/camera.ffm'
+    print(cmd_ffserv)
     # run stream server
-    proc_ffserv = subprocess.Popen(['ffserver', '-f ' + dir_path + '/../../camera/ffserver-mjpeg.conf'])
+    proc_ffserv = subprocess.Popen(cmd_ffserv, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     # wait before run stream process
     sleep(3)
-    proc_ff = subprocess.Popen(['ffmpeg', '-f video4linux2 -s 640x480 -r 15 -i /dev/video0 '
-                                          'http://localhost:8090/camera.ffm'])
+    proc_ff = subprocess.Popen(cmd_ff)
 
     # run car
     car = Car()
