bd317330819460d2f2e1b22b7afc5a42d5da3512
smileexx <smilee900@gmail.com>
2018-11-02 00:29:04 +0200

    fix

--- src/server/main.py
+++ src/server/main.py
@@ -4,10 +4,11 @@ from time import sleep
 
 if __name__ == "__main__":
     # run stream server
-    proc_ffserv = subprocess.Popen('ffserver -f ../../ffserver-mjpeg.conf')
+    proc_ffserv = subprocess.Popen(['ffserver', '-f ../../ffserver-mjpeg.conf'])
     # wait before run stream process
     sleep(3)
-    proc_ff = subprocess.Popen('ffmpeg -f video4linux2 -s 640x480 -r 15 -i /dev/video0 http://localhost:8090/camera.ffm')
+    proc_ff = subprocess.Popen(['ffmpeg', '-f video4linux2 -s 640x480 -r 15 -i /dev/video0 '
+                                          'http://localhost:8090/camera.ffm'])
 
     # run car
     car = Car()
