7db21efb4d6ec783e9edcd6da6fc6fe0484bdce5
smileexx <smilee900@gmail.com>
2018-11-02 00:39:00 +0200

    fix

--- src/server/main.py
+++ src/server/main.py
@@ -1,10 +1,12 @@
 from car import Car
 import subprocess
 from time import sleep
+import os
 
 if __name__ == "__main__":
+    dir_path = os.path.dirname(__file__)
     # run stream server
-    proc_ffserv = subprocess.Popen(['ffserver', '-f ../../ffserver-mjpeg.conf'])
+    proc_ffserv = subprocess.Popen(['ffserver', '-f ' + dir_path + '/../../camera/ffserver-mjpeg.conf'])
     # wait before run stream process
     sleep(3)
     proc_ff = subprocess.Popen(['ffmpeg', '-f video4linux2 -s 640x480 -r 15 -i /dev/video0 '
